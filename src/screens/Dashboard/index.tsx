d5fbf227acbf0d500f5df2f461b8c52b47c73599
ranielOA <r.a_tw@hotmail.com>
2021-07-13 23:56:44 -0300

    starting header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 
-import { Container, Title } from './styles'
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon
+} from './styles'
 
 export function Dashboard(){
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{ uri: 'https://avatars.githubusercontent.com/u/49030804?v=4'}}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Rodrigo</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name="power"/>
+        </UserWrapper>
+      </Header>
     </Container>
   )
 }
