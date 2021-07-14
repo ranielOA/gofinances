0ee06c539950b8fa5ce6aa1a67abd874eb9d0eb6
ranielOA <r.a_tw@hotmail.com>
2021-07-14 00:57:12 -0300

    stilizing cards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -11,19 +11,17 @@ import {
   UserGreeting,
   UserName,
   Icon,
-  HighlightCards,
-} from './styles';
+  HighlightCards
+} from './styles'
 
-export function Dashboard() {
+export function Dashboard(){
   return (
     <Container>
       <Header>
         <UserWrapper>
           <UserInfo>
             <Photo
-              source={{
-                uri: 'https://avatars.githubusercontent.com/u/18651241?s=60&v=4',
-              }}
+              source={{ uri: 'https://avatars.githubusercontent.com/u/49030804?v=4'}}
             />
             <User>
               <UserGreeting>Olá,</UserGreeting>
@@ -31,15 +29,30 @@ export function Dashboard() {
             </User>
           </UserInfo>
 
-          <Icon name="power" />
+          <Icon name="power"/>
         </UserWrapper>
       </Header>
 
       <HighlightCards>
-        <HighlightCard />
-        <HighlightCard />
-        <HighlightCard />
+        <HighlightCard
+          type="up"
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighlightCard
+          type="down"
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última saída dia 13 de abril"
+        />
+        <HighlightCard
+          type="total"
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="01 à 16 de abril"
+        />
       </HighlightCards>
     </Container>
-  );
+  )
 }
