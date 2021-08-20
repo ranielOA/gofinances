c1c82eb2a04ee5d718957c48c8420c164b28d6ac
ranielOA <r.a_tw@hotmail.com>
2021-08-20 00:22:49 -0300

    creating chart and month selector

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,9 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { VictoryPie } from 'victory-native';
+import { useTheme } from 'styled-components';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 import { HistoryCard } from '../../components/HistoryCard';
 
-import { Container, Header, Title, Content } from './styles';
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  ChartContainer,
+  MonthSelect,
+  MonthSelectButton,
+  MonthSelectIcon,
+  Month,
+} from './styles';
 
 import { categories } from '../utils/categories';
 
@@ -18,12 +31,16 @@ interface TransactionData {
 interface CategoryData {
   key: string;
   name: string;
-  total: string;
+  total: number;
+  totalFormatted: string;
   color: string;
+  percent: string;
 }
 
 export function Resume() {
   const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>();
+
+  const theme = useTheme();
 
   async function loadData() {
     const dataKey = '@gofinances:transactions';
@@ -32,6 +49,13 @@ export function Resume() {
 
     const expensives = responseFormatted.filter(
       (expensive: TransactionData) => expensive.type === 'negative'
+    );
+
+    const expensivesTotal = expensives.reduce(
+      (acumulator: number, expensive: TransactionData) => {
+        return acumulator + Number(expensive.amount);
+      },
+      0
     );
 
     const totalByCategory: CategoryData[] = [];
@@ -46,16 +70,22 @@ export function Resume() {
       });
 
       if (categorySum > 0) {
-        const total = categorySum.toLocaleString('pt-Br', {
+        const totalFormatted = categorySum.toLocaleString('pt-Br', {
           style: 'currency',
           currency: 'BRL',
         });
+
+        const percent = `${((categorySum / expensivesTotal) * 100).toFixed(
+          0
+        )}%`;
 
         totalByCategory.push({
           key: category.key,
           name: category.name,
           color: category.color,
-          total,
+          total: categorySum,
+          totalFormatted,
+          percent,
         });
       }
     });
@@ -74,11 +104,40 @@ export function Resume() {
       </Header>
 
       <Content>
+        <MonthSelect>
+          <MonthSelectButton>
+            <MonthSelectIcon name="chevron-left" />
+          </MonthSelectButton>
+
+          <Month>Maio</Month>
+
+          <MonthSelectButton>
+            <MonthSelectIcon name="chevron-right" />
+          </MonthSelectButton>
+        </MonthSelect>
+
+        <ChartContainer>
+          <VictoryPie
+            data={totalByCategories}
+            colorScale={totalByCategories?.map((category) => category.color)}
+            style={{
+              labels: {
+                fontSize: RFValue(18),
+                fontWeight: 'bold',
+                fill: theme.colors.shape,
+              },
+            }}
+            labelRadius={50}
+            x="percent"
+            y="total"
+          />
+        </ChartContainer>
+
         {totalByCategories?.map((item) => (
           <HistoryCard
             key={item.key}
             title={item.name}
-            amount={item.total}
+            amount={item.totalFormatted}
             color={item.color}
           />
         ))}
