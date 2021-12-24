63a7b45a3a2f937ed624f7700bd707555d112e12
ranielOA <r.a_tw@hotmail.com>
2021-12-23 21:03:40 -0300

    making test with context

--- src/components/Form/Input/input.spec.tsx
+++ src/components/Form/Input/input.spec.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { render } from '@testing-library/react-native';
 
 import { Input } from '.';
+import { ThemeProvider } from 'styled-components/native';
+import theme from '../../../global/styles/theme';
+
+const Providers: React.FC = ({ children }) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+);
 
 describe('Input Component', () => {
   it('must have specific border color when active', () => {
@@ -12,10 +18,13 @@ describe('Input Component', () => {
         keyboardType="email-address"
         autoCorrect={false}
         active={true}
-      />
+      />,
+      {
+        wrapper: Providers,
+      }
     );
 
     const inputComponent = getByTestId('input-email');
-    expect(inputComponent.props.style[0].borderColor).toEqual('#E83F5B');
+    expect(inputComponent.props.style[0].borderColor).toEqual(theme.colors.attention);
   });
 });
