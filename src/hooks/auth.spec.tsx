7e1335633d0b831a93470aa9385a5ce293ec23c9
ranielOA <r.a_tw@hotmail.com>
2022-03-23 02:08:20 -0300

    personalized mocks

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -1,6 +1,9 @@
 import { act, renderHook } from '@testing-library/react-hooks';
 import { AuthProvider, useAuth } from './auth';
 import fetchMock from 'jest-fetch-mock';
+import { startAsync } from 'expo-auth-session';
+import { mocked } from 'ts-jest/utils';
+// import { mocked } from 'jest-mock';
 
 fetchMock.enableMocks();
 
@@ -11,19 +14,19 @@ const userTest = {
   photo: 'any_photo.png',
 };
 
-jest.mock('expo-auth-session', () => {
-  return {
-    startAsync: () => ({
+jest.mock('expo-auth-session');
+
+describe('Auth Hook', () => {
+  it('should be able to sign in with Google account existing', async () => {
+    const googleMocked = mocked(startAsync as any);
+
+    googleMocked.mockReturnValueOnce({
       type: 'success',
       params: {
         access_token: 'any_token',
       },
-    }),
-  };
-});
+    });
 
-describe('Auth Hook', () => {
-  it('should be able to sign in with Google account existing', async () => {
     fetchMock.mockResponseOnce(JSON.stringify(userTest));
 
     const { result } = renderHook(() => useAuth(), {
@@ -32,8 +35,24 @@ describe('Auth Hook', () => {
 
     await act(() => result.current.signInWithGoogle());
 
-    console.log('USER PROFILE =>', result.current.user);
-
     expect(result.current.user.email).toBe(userTest.email);
+  });
+
+  it('user should not connect if cancel authentication with Google', async () => {
+    const googleMocked = mocked(startAsync as any);
+
+    googleMocked.mockReturnValueOnce({
+      type: 'cancel',
+    });
+
+    fetchMock.mockResponseOnce(JSON.stringify(userTest));
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    await act(() => result.current.signInWithGoogle());
+
+    expect(result.current.user).not.toHaveProperty('id');
   });
 });
